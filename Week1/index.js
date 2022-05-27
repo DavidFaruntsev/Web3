0e8f4fe50f69ff577cc07718e1b5de90edb5269c
DavidFaruntsev <81746948+DavidFaruntsev@users.noreply.github.com>
2022-05-27 17:12:49 +0200

    update

--- Week1/index.js
+++ Week1/index.js
@@ -13,13 +13,91 @@ function generateLetters(numOfChar)
         }
     }
     return result;
-
-
 }
 
-function openCard(elem)
-{
-    elem.className = 'open-slot';
+// function openCard(elem)
+// {
+//     elem.className = 'open-slot';
+// }
+
+let clickedCard = null;
+let preventClick = false;
+
+
+const colors = [
+    'red',
+    'blue',
+    'violet',
+    'green',
+    'oranje',
+    'black',
+    'yellow',
+    'pink',
+    'teal',
+    'aqua',
+    'purple',
+    'brown',
+    'navy',
+    'lime',
+    'coral',
+    'grey',
+    'steelblue',
+    'olive'
+];
+
+const cards = [...document.querySelectorAll('.slot')];
+for (let color of colors) {
+    const cardAIndex = parseInt(Math.random() * cards.length);
+    const cardA = cards[cardAIndex];
+    cards.splice(cardAIndex, 1);
+    cardA.className += ` ${color}`;
+    cardA.setAttribute('data-color', color);
+
+    const cardBIndex = parseInt(Math.random() * cards.length);
+    const cardB = cards[cardBIndex];
+    cards.splice(cardBIndex, 1);
+    cardB.className += ` ${color}`;
+    cardB.setAttribute('data-color', color);
+}
+
+
+function onCardClicked(e) {
+    const target = e.currentTarget;
+
+    if (
+        preventClick ||
+        target === clickedCard ||
+        target.className.includes('done')) {
+        return;
+    }
+
+    target.className = target.className
+        .replace('color-hidden', '')
+        .trim();
+    target.className += ' done';
+
+    if (!clickedCard) {
+        clickedCard = target;
+    } else if (clickedCard) {
+        if (
+            clickedCard.getAttribute('data-color') !==
+            target.getAttribute('data-color')
+        ) {
+            preventClick = true;
+            setTimeout(() => {
+                clickedCard.className =
+                    clickedCard.className.replace('done', '').trim() +
+                    ' color-hidden';
+                target.className =
+                    target.className.replace('done', '').trim() +
+                    ' color-hidden';
+                clickedCard = null;
+                preventClick = false;
+            }, 500);
+        } else {
+            clickedCard = null;
+        }
+    }
 }
 
 function newGame()
