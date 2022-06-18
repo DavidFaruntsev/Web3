010d8f1a42f96dab43b193ff51bcefe4ac8b9e40
bastiaan <b.meijer@st.hanze.nl>
2022-06-18 13:09:23 +0200

    added ability to register players

---
+++ memory/assets/scripts/register.js
@@ -0,0 +1,25 @@
+const registerUser = () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+
+    const data = {
+        'username': username,
+        'email': email,
+        'password': password
+    }
+
+    fetch('http://localhost:8000/register', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    }).then(res => {
+        console.log('User successfully created')
+    })
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener('click', registerUser);
+
+
